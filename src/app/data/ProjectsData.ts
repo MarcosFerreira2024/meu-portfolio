9494a5741c89a924a8c0d8fb687e5ce5781b2674
Marcos Vinicius Ferreira Pinto <marcosfp2021@hotmail.com>
2025-04-28 14:17:44 -0300

    project object

---
+++ src/app/data/ProjectsData.ts
@@ -0,0 +1,98 @@
+export const projetos = [
+    {
+        id: 1,
+        img: '/projetos/burnout.png',
+        title: 'Burnout',
+        text: `O Burnout é uma plataforma de eCommerce moderna 
+        desenvolvida para oferecer uma experiência de compra rápida, 
+        intuitiva e responsiva. Construído com React, Next.js, Tailwind, 
+        TypeScript e JavaScript, o projeto une o que há de mais recente em tecnologia web para entregar
+         alta performance e design minimalista.`,
+        tecnologias: ['React', 'Next.js', 'Tailwind', 'TypeScript','Node.js'],
+        tec_icons: ['/skills/react.svg', '/skills/nextjs.svg', '/skills/tailwind.svg', '/skills/typescript.svg','skills/nodejs.svg'],
+        live: 'https://burnout-eight.vercel.app/',
+    },
+    {
+        id: 2,
+        img: '/projetos/unifeso.png',
+        title: 'Agenda NAF ',
+        text: `A Agenda NAF UNIFESO é uma plataforma digital 
+        desenvolvida para facilitar o agendamento de atendimentos 
+        no Núcleo de Apoio Contábil e Fiscal (NAF) do Centro Universitário UNIFESO.
+         Criada com foco em praticidade, organização e acessibilidade, a ferramenta moderniza a gestão de horários e 
+        oferece uma experiência simples e eficiente tanto para alunos quanto para a comunidade.`,
+        tecnologias: ['React', 'Tailwind', 'TypeScript'],
+        tec_icons: ['/skills/react.svg', '/skills/tailwind.svg', '/skills/typescript.svg'],
+        live: 'https://projeto-unifeso.vercel.app/',
+    },
+    {
+        id: 3,
+        img: '/projetos/awax.png',
+        title: 'Awax',
+        text: `O Awax é um projeto focado em demonstrar habilidades práticas de desenvolvimento frontend utilizando HTML e CSS3.
+         A proposta é criar uma landing page moderna, responsiva e visualmente atraente,
+         aplicando conceitos fundamentais de estruturação de páginas web e estilização profissional.`,
+        tecnologias: ['HTML', 'CSS', 'JavaScript'],
+        tec_icons: ['/skills/html.svg', '/skills/css.svg', '/skills/javascript.svg'],
+        live: "https://projeto-awax-marcosferreira2024s-projects.vercel.app/",
+    },
+    {
+        id: 4,
+        img: '/projetos/b7-burguer.png',
+        title: 'B7-Burguer',
+        text: `O B7-Burguer é uma landing page desenvolvida para promover e 
+        vender hambúrgueres artesanais de forma direta e impactante. 
+        Criado utilizando HTML e CSS3, 
+        o projeto foca em apresentar o produto de maneira apetitosa, 
+        com um design moderno, responsivo e voltado para conversão.`,
+        tecnologias: ['HTML', 'CSS', 'JavaScript'],
+        tec_icons: ['/skills/html.svg', '/skills/css.svg', '/skills/javascript.svg'],
+        live: "https://b7-burguer-rouge.vercel.app/",
+    },
+    {
+        id: 5,
+        img: '/projetos/portfolio2.png',
+        title: 'Portfolio 2025',
+        text: `Este portfólio foi desenvolvido utilizando Next para a estrutura do projeto e React no frontend, focando na construção de um ambiente moderno e organizado para a apresentação de projetos e habilidades.
+Atualmente, o projeto está concentrado no frontend, utilizando o Next como base de organização (estrutura e rotas básicas), mas sem lógica de backend implementada ainda.
+O layout é limpo, responsivo e pensado para facilitar a navegação e visualização de informações de maneira prática.`,
+        tecnologias: ['React', 'Next.js', 'Tailwind'],
+        tec_icons: ['/skills/react.svg', '/skills/nextjs.svg', '/skills/tailwind.svg'],
+        live: '',
+    },
+    {
+        id: 6,
+        img: '/projetos/portfolio1.png',
+        title: 'Portfolio Next.js',
+        text: 'Este portfólio foi desenvolvido utilizando Next.js, TypeScript e Tailwind, focando em performance, responsividade e experiência de usuário.',
+        tecnologias: ['Next.js', 'TypeScript', 'Tailwind'],
+        tec_icons: ['/skills/nextjs.svg', '/skills/typescript.svg', '/skills/tailwind.svg'],
+        live: "https://meu-portfolio-next.vercel.app/",
+    },
+    {
+        id: 7,
+        img: '/projetos/calculadora.png',
+        title: 'Calculadora IMC',
+        text: `A Calculadora de IMC é um projeto web desenvolvido com TypeScript, Tailwind, HTML e JavaScript,
+         com foco em calcular de forma precisa e elegante o Índice de Massa Corporal (IMC) de um usuário.`,
+        tecnologias: ['TypeScript', 'Tailwind', 'HTML'],
+        tec_icons: ['/skills/typescript.svg', '/skills/tailwind.svg', '/skills/html.svg'],
+        live:"https://calculadora-imc-marcosferreira2024s-projects.vercel.app/",
+    },
+    {
+        id: 8,
+        img: '/projetos/pokedex.png',
+        title: 'Pokédex',
+        text: `A Pokédex é um projeto web interativo criado para listar, 
+        pesquisar e visualizar informações sobre Pokémons de forma rápida e intuitiva. 
+        Desenvolvida com HTML, CSS3 e JavaScript, a plataforma conecta-se a uma 
+        API pública de Pokémons para trazer 
+        dados dinâmicos e atualizados, 
+        entregando uma experiência divertida e educativa para fãs da franquia.`,
+        tecnologias: ['HTML', 'CSS', 'JavaScript'],
+        tec_icons: ['/skills/html.svg', '/skills/css.svg', '/skills/javascript.svg'],
+        live:"https://pokedex-steel-six.vercel.app/",
+    },
+];
+
+export const qntdProjetos = projetos.length
