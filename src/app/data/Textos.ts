c80489ac02adec898e0ef94030b383ffefd11f93
Marcos Vinicius Ferreira Pinto <marcosfp2021@hotmail.com>
2025-04-28 14:17:15 -0300

    resumo

---
+++ src/app/data/Textos.ts
@@ -0,0 +1,5 @@
+export const Resumo = {
+    title: 'Resumo',
+    text: `Olá, Meu nome é Marcos, tenho 21 anos e sou um Desenvolvedor Full-Stack em busca de uma oportunidade de Estágio, tenho expertise em PHP, JavaScript e em banco de dados relacionais e não relacionais. Projeto designs focados na UX e UI com foco no Mobile-First. 
+    Atualmente estou expandido meu conhecimento em Laravel, criando aplicações e em busca de conquistar meu primeiro emprego.`
+}
