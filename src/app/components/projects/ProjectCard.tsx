55fcf53a47193143f710ce0f42cd20f5d99c8f82
Marcos Vinicius Ferreira Pinto <marcosfp2021@hotmail.com>
2025-09-30 05:30:48 -0300

    adding new projects / images
     new photo for backend projects and changing the json projects data

--- src/app/components/projects/ProjectCard.tsx
+++ src/app/components/projects/ProjectCard.tsx
@@ -1,43 +1,38 @@
-"use client"
+"use client";
 
-import React from 'react'
-import Button from '../ui/Button'
-import SkillList from '../skills/SkillList'
-import Image from 'next/image'
-import { ProjectData } from '@/app/hooks/useProjects'
+import React from "react";
+import Button from "../ui/Button";
+import SkillList from "../skills/SkillList";
+import Image from "next/image";
+import { ProjectData } from "@/app/hooks/useProjects";
 
-type ProjectCardProps= {
-    data:ProjectData,
-    increase:()=>void,
-    decrease:()=>void
+type ProjectCardProps = {
+  data: ProjectData;
+  increase: () => void;
+  decrease: () => void;
+};
 
-}
-
-
-
-
-
-function ProjectCard({data,decrease,increase}:ProjectCardProps) {
-  
-  
-
-
-    return (
-    <section className='flex flex-col justify-between gap-5 md:pr-5'>
-        <div className='relative w-full'>
-            <Image width={1200} height={400} src={data.img} alt={data.title} className='object-cover  md:w-full object-center border-2 border-main mainShadow  ' />
-            <div className='absolute bottom-0 left-0 right-0 w-full h-[40px] flex items-center justify-center bg-main-contrast border-2 border-main mainShadow'>
-                <SkillList data={data} />
-
-            </div>
+function ProjectCard({ data, decrease, increase }: ProjectCardProps) {
+  return (
+    <section className="flex flex-col justify-between gap-5 md:pr-5">
+      <div className="relative w-full">
+        <Image
+          width={1200}
+          height={400}
+          src={data.img}
+          alt={data.title}
+          className="object-cover  md:w-full object-center border-2 border-main mainShadow  "
+        />
+        <div className="absolute bottom-0 left-0 right-0 w-full h-[40px] flex items-center justify-center bg-main-contrast border-2 border-main mainShadow">
+          <SkillList data={data} />
         </div>
-        <nav className='flex  gap-2 min-w-[60px] justify-center md:justify-end'>
-            <Button src="/icons/prev.svg" alt="Voltar" action={decrease} />
-            <Button src='/icons/next.svg' alt='Avançar' action={increase}/>
-        </nav>
-      
+      </div>
+      <nav className="flex  gap-2 min-w-[60px] justify-center md:justify-end">
+        <Button src="/icons/prev.svg" alt="Voltar" action={decrease} />
+        <Button src="/icons/next.svg" alt="Avançar" action={increase} />
+      </nav>
     </section>
-  )
+  );
 }
 
-export default ProjectCard
+export default ProjectCard;
--- src/app/data/ProjectsData.ts
+++ src/app/data/ProjectsData.ts
@@ -1,98 +1,132 @@
 export const projetos = [
-    {
-        id: 1,
-        img: '/projetos/burnout.png',
-        title: 'Burnout',
-        text: `O Burnout é uma plataforma de eCommerce moderna 
+  {
+    id: 1,
+    img: "/projetos/backend.png",
+    title: "Y-Backend",
+    text: `Projeto desenvolvido com foco em Domain-Driven Design (DDD), estruturado em
+módulos e camadas (domain, application/useCases, infrastructure).
+Os casos de uso (UseCases) foram implementados para organizar a lógica de
+negócio de forma isolada e reutilizável.
+O sistema utilizou interfaces e implementações, aplicando o princípio de Inversão de
+Dependência para manter baixo acoplamento e facilitar a manutenção.
+A arquitetura priorizou código limpo, escalabilidade e testabilidade, demonstrando
+domínio de boas práticas avançadas de backend.`,
+    tecnologias: ["Node.js", "TypeScript"],
+    tec_icons: ["skills/nodejs.svg", "/skills/typescript.svg"],
+    live: "https://github.com/MarcosFerreira2024/Burnout",
+  },
+  {
+    id: 2,
+    img: "/projetos/backend.png",
+    title: "GameDatabase",
+    text: `O GameDatabase é um projeto backend desenvolvido em Node.js e TypeScript,
+    integrado à API externa IGDB. O sistema foi estruturado de forma modular,
+    aplicando Programação Orientada a Objetos (POO), casos de uso (UseCases),
+    interfaces e implementações, além do princípio de Inversão de Dependência.
+    A arquitetura foi projetada para ser escalável, manutenível e de fácil expansão,
+    servindo como base sólida para futuras integrações com clientes web ou mobile.`,
+    tecnologias: ["Node.js", "TypeScript"],
+    tec_icons: ["/skills/nodejs.svg", "/skills/typescript.svg"],
+
+    live: "https://github.com/MarcosFerreira2024/Burnout",
+  },
+
+  {
+    id: 3,
+    img: "/projetos/burnout.png",
+    title: "Burnout",
+    text: `O Burnout é uma plataforma de eCommerce moderna 
         desenvolvida para oferecer uma experiência de compra rápida, 
         intuitiva e responsiva. Construído com React, Next.js, Tailwind, 
         TypeScript e JavaScript, o projeto une o que há de mais recente em tecnologia web para entregar
          alta performance e design minimalista.`,
-        tecnologias: ['React', 'Next.js', 'Tailwind', 'TypeScript','Node.js'],
-        tec_icons: ['/skills/react.svg', '/skills/nextjs.svg', '/skills/tailwind.svg', '/skills/typescript.svg','skills/nodejs.svg'],
-        live: 'https://github.com/MarcosFerreira2024/Burnout',
-    },
-    {
-        id: 2,
-        img: '/projetos/unifeso.png',
-        title: 'Agenda NAF ',
-        text: `A Agenda NAF UNIFESO é uma plataforma digital 
+    tecnologias: ["React", "Next.js", "Tailwind", "TypeScript", "Node.js"],
+    tec_icons: [
+      "/skills/react.svg",
+      "/skills/nextjs.svg",
+      "/skills/tailwind.svg",
+      "/skills/typescript.svg",
+      "skills/nodejs.svg",
+    ],
+    live: "https://github.com/MarcosFerreira2024/Burnout",
+  },
+  {
+    id: 4,
+    img: "/projetos/unifeso.png",
+    title: "Agenda NAF ",
+    text: `A Agenda NAF UNIFESO é uma plataforma digital 
         desenvolvida para facilitar o agendamento de atendimentos 
         no Núcleo de Apoio Contábil e Fiscal (NAF) do Centro Universitário UNIFESO.
          Criada com foco em praticidade, organização e acessibilidade, a ferramenta moderniza a gestão de horários e 
         oferece uma experiência simples e eficiente tanto para alunos quanto para a comunidade.`,
-        tecnologias: ['React', 'Tailwind', 'TypeScript'],
-        tec_icons: ['/skills/react.svg', '/skills/tailwind.svg', '/skills/typescript.svg'],
-        live: 'https://github.com/MarcosFerreira2024/Projeto-Unifeso',
-    },
-    {
-        id: 3,
-        img: '/projetos/awax.png',
-        title: 'Awax',
-        text: `O Awax é um projeto focado em demonstrar habilidades práticas de desenvolvimento frontend utilizando HTML e CSS3.
+    tecnologias: ["React", "Tailwind", "TypeScript"],
+    tec_icons: [
+      "/skills/react.svg",
+      "/skills/tailwind.svg",
+      "/skills/typescript.svg",
+    ],
+    live: "https://github.com/MarcosFerreira2024/Projeto-Unifeso",
+  },
+  {
+    id: 5,
+    img: "/projetos/awax.png",
+    title: "Awax",
+    text: `O Awax é um projeto focado em demonstrar habilidades práticas de desenvolvimento frontend utilizando HTML e CSS3.
          A proposta é criar uma landing page moderna, responsiva e visualmente atraente,
          aplicando conceitos fundamentais de estruturação de páginas web e estilização profissional.`,
-        tecnologias: ['HTML', 'CSS', 'JavaScript'],
-        tec_icons: ['/skills/html.svg', '/skills/css.svg', '/skills/javascript.svg'],
-        live: "https://github.com/MarcosFerreira2024/Projeto-Awax",
-    },
-    {
-        id: 4,
-        img: '/projetos/b7-burguer.png',
-        title: 'B7-Burguer',
-        text: `O B7-Burguer é uma landing page desenvolvida para promover e 
+    tecnologias: ["HTML", "CSS", "JavaScript"],
+    tec_icons: [
+      "/skills/html.svg",
+      "/skills/css.svg",
+      "/skills/javascript.svg",
+    ],
+    live: "https://github.com/MarcosFerreira2024/Projeto-Awax",
+  },
+  {
+    id: 6,
+    img: "/projetos/b7-burguer.png",
+    title: "B7-Burguer",
+    text: `O B7-Burguer é uma landing page desenvolvida para promover e 
         vender hambúrgueres artesanais de forma direta e impactante. 
         Criado utilizando HTML e CSS3, 
         o projeto foca em apresentar o produto de maneira apetitosa, 
         com um design moderno, responsivo e voltado para conversão.`,
-        tecnologias: ['HTML', 'CSS', 'JavaScript'],
-        tec_icons: ['/skills/html.svg', '/skills/css.svg', '/skills/javascript.svg'],
-        live: "https://github.com/MarcosFerreira2024/B7-Burguer",
-    },
-    {
-        id: 5,
-        img: '/projetos/portfolio2.png',
-        title: 'Portfolio 2025',
-        text: `Este portfólio foi desenvolvido utilizando Next para a estrutura do projeto e React no frontend, focando na construção de um ambiente moderno e organizado para a apresentação de projetos e habilidades.
+    tecnologias: ["HTML", "CSS", "JavaScript"],
+    tec_icons: [
+      "/skills/html.svg",
+      "/skills/css.svg",
+      "/skills/javascript.svg",
+    ],
+    live: "https://github.com/MarcosFerreira2024/B7-Burguer",
+  },
+  {
+    id: 7,
+    img: "/projetos/portfolio2.png",
+    title: "Portfolio 2025",
+    text: `Este portfólio foi desenvolvido utilizando Next para a estrutura do projeto e React no frontend, focando na construção de um ambiente moderno e organizado para a apresentação de projetos e habilidades.
 Atualmente, o projeto está concentrado no frontend, utilizando o Next como base de organização (estrutura e rotas básicas), mas sem lógica de backend implementada ainda.
 O layout é limpo, responsivo e pensado para facilitar a navegação e visualização de informações de maneira prática.`,
-        tecnologias: ['React', 'Next.js', 'Tailwind'],
-        tec_icons: ['/skills/react.svg', '/skills/nextjs.svg', '/skills/tailwind.svg'],
-        live: 'https://github.com/MarcosFerreira2024/meu-portfolio',
-    },
-    {
-        id: 6,
-        img: '/projetos/portfolio1.png',
-        title: 'Portfolio Next.js',
-        text: 'Este portfólio foi desenvolvido utilizando Next.js, TypeScript e Tailwind, focando em performance, responsividade e experiência de usuário.',
-        tecnologias: ['Next.js', 'TypeScript', 'Tailwind'],
-        tec_icons: ['/skills/nextjs.svg', '/skills/typescript.svg', '/skills/tailwind.svg'],
-        live: "https://github.com/MarcosFerreira2024/portfolio",
-    },
-    {
-        id: 7,
-        img: '/projetos/calculadora.png',
-        title: 'Calculadora IMC',
-        text: `A Calculadora de IMC é um projeto web desenvolvido com TypeScript, Tailwind, HTML e JavaScript,
-         com foco em calcular de forma precisa e elegante o Índice de Massa Corporal (IMC) de um usuário.`,
-        tecnologias: ['TypeScript', 'Tailwind', 'HTML'],
-        tec_icons: ['/skills/typescript.svg', '/skills/tailwind.svg', '/skills/html.svg'],
-        live:"https://github.com/MarcosFerreira2024/Calculadora-Imc",
-    },
-    {
-        id: 8,
-        img: '/projetos/pokedex.png',
-        title: 'Pokédex',
-        text: `A Pokédex é um projeto web interativo criado para listar, 
-        pesquisar e visualizar informações sobre Pokémons de forma rápida e intuitiva. 
-        Desenvolvida com HTML, CSS3 e JavaScript, a plataforma conecta-se a uma 
-        API pública de Pokémons para trazer 
-        dados dinâmicos e atualizados, 
-        entregando uma experiência divertida e educativa para fãs da franquia.`,
-        tecnologias: ['HTML', 'CSS', 'JavaScript'],
-        tec_icons: ['/skills/html.svg', '/skills/css.svg', '/skills/javascript.svg'],
-        live:"https://github.com/MarcosFerreira2024/Pokedex",
-    },
+    tecnologias: ["React", "Next.js", "Tailwind"],
+    tec_icons: [
+      "/skills/react.svg",
+      "/skills/nextjs.svg",
+      "/skills/tailwind.svg",
+    ],
+    live: "https://github.com/MarcosFerreira2024/meu-portfolio",
+  },
+  {
+    id: 8,
+    img: "/projetos/portfolio1.png",
+    title: "Portfolio Next.js",
+    text: "Este portfólio foi desenvolvido utilizando Next.js, TypeScript e Tailwind, focando em performance, responsividade e experiência de usuário.",
+    tecnologias: ["Next.js", "TypeScript", "Tailwind"],
+    tec_icons: [
+      "/skills/nextjs.svg",
+      "/skills/typescript.svg",
+      "/skills/tailwind.svg",
+    ],
+    live: "https://github.com/MarcosFerreira2024/portfolio",
+  },
 ];
 
-export const qntdProjetos = projetos.length
+export const qntdProjetos = projetos.length;
