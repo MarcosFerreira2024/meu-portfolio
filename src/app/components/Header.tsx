a5a4534f65bfc8ad761267ea73bd54eec105f5c1
Marcos Vinicius Ferreira Pinto <marcosfp2021@hotmail.com>
2025-10-07 16:55:39 -0300

    Revert "Creating new animations"
    This reverts commit 29b9baef20dbcde66687870fec1861544fd13a91.

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,67 +1,47 @@
-"use client";
+"use client"
 
-import Image from "next/image";
-import React from "react";
+import Image from 'next/image';
+import React from 'react'
 
-import { motion } from "framer-motion";
+
+
+
 
 function Header() {
-  const handleEasterEgg = () => {
-    document.body.classList.toggle("rotate-[-2deg]");
-    document.body.classList.toggle("overflow-x-hidden");
-    document.body.classList.toggle("overflow-y-hidden");
 
-    if (document.body.classList.contains("rotate-[-2deg]")) {
+  const handleEasterEgg = () => {
+    
+    document.body.classList.toggle('rotate-[-2deg]',);
+    document.body.classList.toggle('overflow-x-hidden');
+    document.body.classList.toggle('overflow-y-hidden');
+
+    if(document.body.classList.contains('rotate-[-2deg]')) {
+      
       setTimeout(() => {
-        document.body.classList.remove("rotate-[-2deg]");
+        document.body.classList.remove('rotate-[-2deg]');
+
       }, 1000);
 
-      setTimeout(() => {
-        document.body.classList.remove("overflow-y-hidden");
-        document.body.classList.remove("overflow-x-hidden");
-      }, 1400);
+
+      setTimeout(()=>{
+        document.body.classList.remove('overflow-y-hidden')
+        document.body.classList.remove('overflow-x-hidden');
+
+      },1400)
     }
-  };
+
+    
+  }
+
+
 
   return (
-    <header className="flex flex-col w-full justify-center items-center text-center pt-5 border-b-2 border-main relative">
-      <motion.p
-        initial={{ y: -20, opacity: 0 }}
-        animate={{ y: 0, opacity: 1 }}
-        transition={{ duration: 0.3, delay: 0.1, ease: "easeInOut" }}
-        className="text-main"
-      >
-        Desenvolvedor Full-Stack
-      </motion.p>
-      <motion.h1
-        initial={{ y: -10, opacity: 0 }}
-        animate={{ y: 0, opacity: 1 }}
-        transition={{ duration: 0.3, ease: "easeInOut" }}
-        className="md:text-gg text-4xl text-main-contrast "
-      >
-        Marcos Vinicius Ferreira Pinto{" "}
-      </motion.h1>
-      <motion.div
-        className="absolute right-2 top-2"
-        initial={{ rotate: 0 }}
-        animate={{ rotate: [25, 0, 25] }}
-        transition={{
-          duration: 0.5,
-          ease: "linear",
-        }}
-      >
-        <Image
-          width={24}
-          height={24}
-          onClick={handleEasterEgg}
-          src="/icons/wave.svg"
-          className="duration-300 ease-in-out hover:rotate-12   max-w-[24px] md:max-w-[48px] cursor-not-allowed"
-          alt="Não clique em mim"
-          title="Não clique em mim"
-        />
-      </motion.div>
+    <header className='flex flex-col w-full justify-center items-center text-center pt-5 border-b-2 border-main relative'>
+        <p className='text-main'>Desenvolvedor Full-Stack</p>
+        <h1 className='md:text-gg text-4xl text-main-contrast '>Marcos Vinicius Ferreira Pinto </h1>
+        <Image width={24} height={24} onClick={handleEasterEgg} src="/icons/wave.svg" className='duration-300 ease-in-out hover:rotate-12 absolute right-2 top-2  max-w-[24px] md:max-w-[48px] cursor-not-allowed' alt="Não clique em mim" title='Não clique em mim'  />
     </header>
-  );
+  )
 }
 
-export default Header;
+export default Header
